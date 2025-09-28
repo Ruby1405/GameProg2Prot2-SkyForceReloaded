bbc7696a48bd0f3ce62cee05d6a39d244ef83d98
Ruby1405 <ruby.calmerman@gmail.com>
2025-09-28 22:58:57 +0200

    flying, tilting plane

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private Vector2 currentDirection = Vector2.zero;
+    private Vector2 currentAcceleration = Vector2.zero;
+    [SerializeField] private float acceleration = 20f;
+    [SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private UnityEvent<Vector2> onAccelerationChange;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void GetMoveInput(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    void Move()
+    {
+        rb.AddForce(new Vector3(moveInput.x, 0, moveInput.y) * acceleration * Time.fixedDeltaTime, ForceMode.Acceleration);
+
+        if (rb.linearVelocity.x > maxSpeed)
+            rb.linearVelocity = new Vector3(maxSpeed, rb.linearVelocity.y, rb.linearVelocity.z);
+        if (rb.linearVelocity.x < -maxSpeed)
+            rb.linearVelocity = new Vector3(-maxSpeed, rb.linearVelocity.y, rb.linearVelocity.z);
+        if (rb.linearVelocity.z > maxSpeed)
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, maxSpeed);
+        if (rb.linearVelocity.z < -maxSpeed)
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, -maxSpeed);
+
+        Vector2 newDirection = new Vector2(rb.linearVelocity.x, rb.linearVelocity.z).normalized;
+
+        if (currentDirection != newDirection)
+        {
+            currentDirection = newDirection;
+        }
+
+        if (currentAcceleration != moveInput)
+        {
+            onAccelerationChange?.Invoke(moveInput);
+            currentAcceleration = moveInput;
+        }
+    }
+}
---
+++ Assets/Scripts/TiltMeshAnimator.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TiltMeshAnimator : MonoBehaviour
+{
+    [SerializeField] private Transform meshTransform;
+    [SerializeField] private float verticalLeanAngle = 60f;
+    [SerializeField] private float horizontalLeanAngle = 60f;
+    [SerializeField] private float tiltSpeed = 5f;
+    private Vector2 currentTarget = Vector2.zero;
+    private Quaternion initialRotation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        initialRotation = meshTransform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion targetRotation = initialRotation;
+        if (currentTarget != Vector2.zero)
+        {
+            targetRotation = Quaternion.Euler(-currentTarget.y * verticalLeanAngle, 0, currentTarget.x * horizontalLeanAngle) * initialRotation;
+        }
+        meshTransform.localRotation = Quaternion.Slerp(meshTransform.localRotation, targetRotation, Time.deltaTime * tiltSpeed);
+    }
+
+    public void UpdateTargetVector(Vector2 newTarget)
+    {
+        currentTarget = newTarget;
+    }
+}
